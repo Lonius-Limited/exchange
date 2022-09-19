81cba052dff42f261a644787b5e1a4638ce62728
Administrator <info@lonius.co.ke>
2022-09-19 23:26:55 +0300

    Settings doctype added and credential creation api

---
+++ exchange/api/credentials.py
@@ -0,0 +1,31 @@
+import frappe
+
+@frappe.whitelist()
+def create_credentials(email, facility_name):
+	frappe.only_for("System Manager")
+	if frappe.db.exists("User", email, cache=True):
+		return generate_api_keys(email)
+	user = frappe.get_doc(
+		dict(doctype="User", email=email, first_name=facility_name, send_welcome_email = False, user_type = "System User")
+	)
+	user.db_insert()
+
+	# user = frappe.new_doc("User")
+	# user.email = email
+	# user.send_welcome_email = False
+	# user.first_name = facility_name
+	# user.name = email
+	# user.user_type = "System User"
+	user.append('roles',dict(role='System Manager'))
+	user.save(ignore_permissions=True)
+	return generate_api_keys(user)
+
+def generate_api_keys(user):
+	user_details = frappe.get_doc("User", user)
+	api_secret = frappe.generate_hash(length=15)
+	if not user_details.api_key:
+		api_key = frappe.generate_hash(length=15)
+		user_details.api_key = api_key
+	user_details.api_secret = api_secret
+	user_details.save()
+	return user_details
