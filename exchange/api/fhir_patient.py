ff75ed0b4db0ce53b3191fd19f9343bda86e5335
Administrator <info@lonius.co.ke>
2022-09-26 22:37:03 +0300

    completed creation and updating of patient on fhir server

---
+++ exchange/api/fhir_patient.py
@@ -0,0 +1,131 @@
+import frappe
+import asyncio
+import json
+import re
+from fhirpy import AsyncFHIRClient
+from fhirpy import SyncFHIRClient
+from fhir.resources.patient import Patient
+from fhir.resources.humanname import HumanName
+from fhir.resources.contactpoint import ContactPoint
+from fhir.resources.identifier import Identifier
+
+def fhir_server_connect():
+	fhir_api_key = frappe.db.get_single_value('Exchange Settings', 'shr_key') or "api_key"
+	fhir_api_secret = frappe.db.get_single_value('Exchange Settings', 'shr_secret') or "api_secret"
+	return SyncFHIRClient(url='http://hie.loniushealth.com:8081/fhir', extra_headers={fhir_api_key:fhir_api_secret})
+
+def validate_date(the_date, throw=False):
+	if not the_date:
+		return False
+	the_date = the_date.strip()
+	match = re.match(r"(?:19\d{2}|20[01][0-9]|2020)[-/.](?:0[1-9]|1[012])[-/.](?:0[1-9]|[12][0-9]|3[01])", the_date)
+	if not match and throw:
+		frappe.throw(
+			frappe._("{0} is not a valid Date. Provide it in the format YYYY-MM-DD").format(the_date)
+		)
+
+	return bool(match)
+
+def update_patient_details(shr, patient, first_name, gender, birth_date, last_name, middle_name, phone, email):
+	patient.name[0].given[0] = first_name
+	patient.name[0].given[1] = middle_name
+	patient.name[0].family = last_name
+	patient.gender = gender
+	validate_date(birth_date, True)
+	patient.birthDate = birth_date
+	patient.telecom[0].value = phone
+	patient.telecom[1].value = email
+	shr.resource('Patient',**json.loads(patient.json())).save()
+	return patient
+
+@frappe.whitelist()
+def register_patient(
+	patient_id = 100,
+	first_name = "John",
+	gender = "female",
+	birth_date = "1990-01-03",
+	last_name = "Doe",
+	middle_name = "Sang",
+	phone = "0777723456",
+	email = ""
+	):
+	shr = fhir_server_connect()
+	patient = Patient()
+
+	#SEARCH FOR PATIENT WITH SAME ID. IF EXISTING, DO AN UPDATE INSTEAD.
+	patients_resources = shr.resources('Patient')
+	patient_with_id_obj = patients_resources.search(identifier__value = patient_id).first()
+	patient_with_id = Patient.parse_obj(patient_with_id_obj.serialize()) if patient_with_id_obj else None
+	if patient_with_id: 
+		return update_patient_details(shr = shr, patient = patient_with_id, first_name = first_name, gender = gender, birth_date = birth_date, last_name = last_name,
+			middle_name = middle_name, phone = phone, email = email or f'{patient_id}@lonius.co.ke'
+		)
+	#SAVE THE ID
+	identifier = Identifier()
+	identifier.value = patient_id
+	identifier.use = "official"
+	patient.identifier = [identifier]
+	
+	#VALIDATE THE DATE AND SAVE
+	validate_date(birth_date, True)
+	patient.birthDate = birth_date
+
+	#SAVE THE GENDER
+	patient.gender = gender
+
+	#SAVE THE NAME
+	name = HumanName()
+	name.use = "official"
+	name.family = last_name or "familyname"
+	name.given = [first_name or "givenname1", middle_name or "givenname2"]
+	patient.name = [name]
+
+	#SAVE THE PHONE AND EMAIL
+	telecom_phone = ContactPoint()
+	telecom_phone.system = 'phone'
+	telecom_phone.use = 'mobile'
+	telecom_phone.value = phone or '0700 123 456'
+
+	#EMAIL
+	if frappe.utils.validate_email_address(email) == '' and email != '':
+		frappe.throw("The email address is not valid")
+	telecom_email = ContactPoint()
+	telecom_email.system = 'email'
+	telecom_email.use = 'home'
+	telecom_email.value = email or f'{patient_id}@lonius.co.ke'
+	patient.telecom = [telecom_phone, telecom_email]
+
+	#SAVE THE RECORD
+	shr.resource('Patient',**json.loads(patient.json())).save()
+	return patient
+
+
+
+
+async def main():
+	# Create an instance
+	client = AsyncFHIRClient(
+		'http://hie.loniushealth.com:8081/fhir',
+		authorization='Bearer TOKEN',
+	)
+
+	# Search for patients
+	# resources = client.resources('Patient')  # Return lazy search set
+	# resources = resources.search(name='John').limit(10).sort('name')
+	# patients = await resources.fetch()  # Returns list of AsyncFHIRResource
+
+	# Create Organization resource
+	organization = client.resource(
+		'Organization',
+		name='Moi Teaching & Referral Hospital',
+		active=True
+	)
+	await organization.save()
+	org_resources = client.resources('Organization')
+	# Lazy loading resources page by page with page count = 100
+	async for org_resource in org_resources.limit(100):
+		print(org_resource.serialize())
+
+def create_organization():
+	loop = asyncio.get_event_loop()
+	loop.run_until_complete(create_organization_async())
